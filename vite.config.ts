59e8cba2bee217ef1db663a0b71a7558c65d15e6
NikBel3476 <nikita.belkin3476@gmail.com>
2023-08-06 16:42:59 +0400

    fix: add 404.html to build resources

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
   base: '/TgTestBotFrontend/',
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'index.html'),
+				notFound: resolve(__dirname, '404.html')
+			}
+		}
+	},
   css: {
     modules: {
       localsConvention: 'camelCaseOnly'
